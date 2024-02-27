b9deebc0f4694009f74220421c5fcdee19f57c6d
kutty023 <arthiaaru23@gmail.com>
2024-02-27 22:21:10 +0530

    added routing

--- Frontend/artworkdisplay/src/App.jsx
+++ Frontend/artworkdisplay/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import LandingPage from './Pages/LandingPage'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage';
+import ArtWorksPage from './Pages/ArtWorksPage';
+import data from './Components/ArtWorksData.json'
 
-function App(){
+function App() {
   return (
-    <>
-      <LandingPage />
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/artworksdisplay" element={<ArtWorksPage artworks={data}/>} />
+      </Routes>
+  );
 }
 
-export default App
+export default App;
